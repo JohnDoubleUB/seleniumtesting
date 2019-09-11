f01049e6f40c85846aae67b29c151b0801f89a04
johndoubleub <up741132@myport.ac.uk>
2019-09-11 10:24:57 +0100

    Added Selenium Test for google!

--- src/test/java/com/qa/GoogleSeleniumTest.java
+++ src/test/java/com/qa/GoogleSeleniumTest.java
@@ -2,14 +2,45 @@ package com.qa;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.Assert.assertTrue;
 
 public class GoogleSeleniumTest {
     private ChromeDriver driver;
 
     @Before
-    public void setup(){
+    public void setUp(){
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Admin\\IdeaProjects\\seleniumtesting\\src\\test\\java\\resources\\chromedriver.exe");
         driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @After
+    public void tearDown(){
+        driver.close();
+    }
+
+    //fake-input
+    @Test
+    public void searchTest() throws InterruptedException {
+        driver.get("http://google.com");
+        Thread.sleep(2000);
+        WebElement searchField = driver.findElement(By.name("q"));
+        WebElement searchButton = driver.findElement(By.name("btnK"));
+        assertTrue(searchField.isDisplayed());
+        searchField.sendKeys("funny cat pictures");
+        Thread.sleep(2000);
+        searchButton.click();
+        Thread.sleep(2000);
+        WebElement linkToBiggerPicture = driver.findElementByLinkText("Images for funny cat");
+        linkToBiggerPicture.click();
+        Thread.sleep(2000);
+
+        Thread.sleep(2000);
     }
 
 }
